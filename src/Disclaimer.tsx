e86b4d788b42299709e4b3451b51264cdc935903
Fabio Luciani <lu.fa.98@gmail.com>
2024-08-26 11:30:41 +0200

    Update Disclaimer.tsx

--- src/Disclaimer.tsx
+++ src/Disclaimer.tsx
@@ -3,12 +3,8 @@ import React from "react";
 function Disclaimer() {
     return (
         <div>
-            <div className="text-4xl font-bold">Basic or Advanced?</div>
+            <div className="text-4xl font-bold">Mapping Evaluation</div>
             <div>
-                <div className="text-2xl mt-4">Scope of this annotation task</div>
-                <p className="mt-2">
-                    The goal of this annotation is to define a <b>gold standard</b> for basic/advanced <b>words</b>.
-                </p>
                 <div className="text-2xl mt-4">What are you going to do?</div>
                 <p className="mt-2">
                     In this annotation task, you will be given a word to evaluate. Your job is
@@ -22,22 +18,6 @@ function Disclaimer() {
                     you. The goal of this task is simply to help us understand how people classify words as basic or
                     advanced. At any point during the annotation task, you can mark a word as "<i>difficult</i>" if you
                     find it challenging to determine whether it is basic or advanced.
-                </p>
-                <div className="text-2xl mt-4">What are basic and advanced words?</div>
-                <p className="mt-2">
-                    <b>There is no precise and objective definition of what a basic word is.</b> However, there are
-                    some general characteristics that most people would agree on. Here's a few:
-                </p>
-                <ul className="mt-2 list-decimal">
-                    <li>Basic words are <b>tools for social survival</b>: they are the Swiss army knife for
-                        second-language-learners (useful for a lot of social interactions between non-native speakers)
-                    </li>
-                    <li>They are usually <b>short and easy to say</b> and to conceptualize</li>
-                    <li>They are the <b>first words that come to mind</b> when speaking about a certain topic</li>
-                    <li>They can be <b>easily translated to a clear mental image</b></li>
-                </ul>
-                <p className="mt-2">
-                    Given the definitions above, an advanced word for a concept is a term that is <b>not basic</b>.
                 </p>
             </div>
         </div>
